d257b6a078422f6e354c6439f5c9a51ee901225f
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2020-03-14 16:50:52 -0400

    Test

---
+++ cjs/index.js
@@ -0,0 +1,101 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _misc = require("./misc");
+
+Object.keys(_misc).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _misc[key];
+    }
+  });
+});
+
+var _users = require("./users");
+
+Object.keys(_users).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _users[key];
+    }
+  });
+});
+
+var _dormtrak = require("./dormtrak");
+
+Object.keys(_dormtrak).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _dormtrak[key];
+    }
+  });
+});
+
+var _factrak = require("./factrak");
+
+Object.keys(_factrak).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _factrak[key];
+    }
+  });
+});
+
+var _bulletins = require("./bulletins");
+
+Object.keys(_bulletins).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _bulletins[key];
+    }
+  });
+});
+
+var _autocomplete = require("./autocomplete");
+
+Object.keys(_autocomplete).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _autocomplete[key];
+    }
+  });
+});
+
+var _auth = require("./auth");
+
+Object.keys(_auth).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _auth[key];
+    }
+  });
+});
+
+var _config = require("./config");
+
+Object.keys(_config).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _config[key];
+    }
+  });
+});
