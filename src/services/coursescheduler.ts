12b10754d4a50d46e41191a16a88eaf4b66b1464
Charlie <charlie@charliemax.dev>
2024-11-10 15:46:03 -0500

    Lint

--- src/services/coursescheduler.ts
+++ src/services/coursescheduler.ts
@@ -1,19 +1,19 @@
 import { API, APIResponse } from '../api';
 import { CourseSelectionSetRequest, ModelsCourseSelectionString } from './types';
 export class CourseSchedulerService {
-    private api: API;
+  private api: API;
 
-    constructor(api: API) {
-        this.api = api;
-    }
+  constructor(api: API) {
+    this.api = api;
+  }
 
-    async getSelections(unixID: number): Promise<APIResponse<ModelsCourseSelectionString>> {
-        return this.api.request('get', '/api/v2/course-selections/' + unixID)
-    }
+  async getSelections(unixID: number): Promise<APIResponse<ModelsCourseSelectionString>> {
+    return this.api.request('get', '/api/v2/course-selections/' + unixID);
+  }
 
-    async setSelection(unixID: number, body: CourseSelectionSetRequest) {
-        return this.api.request('post', '/api/v2/course-selections/' + unixID, {
-            data: body,
-        })
-    }
+  async setSelection(unixID: number, body: CourseSelectionSetRequest) {
+    return this.api.request('post', '/api/v2/course-selections/' + unixID, {
+      data: body,
+    });
+  }
 }
